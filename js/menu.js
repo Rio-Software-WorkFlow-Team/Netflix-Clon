e78688a38c3cb4f72e3be5b3d161abd6345bf64a
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-08-06 22:43:53 -0500

    agregado el segundo menu de la sesion

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,14 @@
 window.addEventListener("scroll", function() {
     const nav = document.querySelector("nav");
+    const subNav = document.querySelector("#subNav");
     const scrollPosition = window.scrollY;
   
     // Agrega o quita la clase "scroll" según el desplazamiento de la página
     if (scrollPosition > 0) {
         nav.classList.add("scroll");
+        subNav.classList.add("subScroll");
     } else {
         nav.classList.remove("scroll");
+        subNav.classList.remove("subScroll");
     }
   });
